0343b7b7a2e64acfe673d8c3dca30cdcfa5e0241
mupadhya-ta <mupadhya@uottawa.ca>
2023-03-16 18:00:15 -0400

    initial commit

---
+++ app.py
@@ -0,0 +1,56 @@
+import os
+from flask import Flask, render_template, request
+import pickle
+import numpy as np
+from PIL import Image
+import tensorflow as tf
+
+app = Flask(__name__)
+
+def predict(values, dic):
+    # diabetes
+    
+    # kidney disease
+    if len(values) == 24:
+        model = pickle.load(open('models/kidney.pkl','rb'))
+        values = np.asarray(values)
+        return model.predict(values.reshape(1, -1))[0]
+
+    
+
+@app.route("/")
+def home():
+    return render_template('home.html')
+
+
+
+@app.route("/kidney", methods=['GET', 'POST'])
+def kidneyPage():
+    return render_template('kidney.html')
+
+
+
+@app.route("/predict", methods = ['POST', 'GET'])
+def predictPage():
+    try:
+        if request.method == 'POST':
+            to_predict_dict = request.form.to_dict()
+
+            for key, value in to_predict_dict.items():
+                try:
+                    to_predict_dict[key] = int(value)
+                except ValueError:
+                    to_predict_dict[key] = float(value)
+
+            to_predict_list = list(map(float, list(to_predict_dict.values())))
+            pred = predict(to_predict_list, to_predict_dict)
+    except:
+        message = "Please enter valid data"
+        return render_template("home.html", message=message)
+
+    return render_template('predict.html', pred=pred)
+
+
+
+if __name__ == '__main__':
+    app.run(debug = True)
